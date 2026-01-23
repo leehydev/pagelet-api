3a7fcc4e5254a2925c8bd31bbf18e9d8730438a0
leehydev <leehydev@gmail.com>
2026-01-24 00:13:07 +0900

    헬스 체크 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  @Public()
+  @ApiTags('Health')
+  @ApiOperation({ summary: 'Health check' })
+  healthCheck(): { status: string } {
+    return { status: 'ok' };
   }
 }
